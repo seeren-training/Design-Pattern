8ea976ee8376d2ec24c8f20ea4ad12bf7ac82c50
seeren <consultant@seeren.fr>
2021-11-15 15:54:22 +0100

    Add manip 1

---
+++ demo-design-pattern/public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+
+/**
+ * Pour les URL
+ *
+ * "/cards" il faut exécuter la méthode "showAll" du CardController
+ * "/cards/3"
+ * "/cards/4"
+ * "/cards/5" il faut exécuter la méthode "show" du CardController
+ * Si possible cette méthode déclare en paramètre entrant d'id capturé
+ */
+
+
+include './../vendor/autoload.php';
+
+$routes = [];
+
+foreach ($routes as $route) {
+
+}
---
+++ demo-design-pattern/src/Controller/CardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+class CardController
+{
+
+    public function showAll()
+    {
+        echo "Show All";
+    }
+
+    public function show($id)
+    {
+
+        echo $id;
+    }
+
+}
