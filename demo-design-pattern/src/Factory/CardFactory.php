bc8334505c0cb467e23b0ea895f974f1f8086625
seeren <consultant@seeren.fr>
2021-11-18 10:34:05 +0100

    Correction prototype

--- demo-design-pattern/src/Factory/CardFactory.php
+++ demo-design-pattern/src/Factory/CardFactory.php
@@ -11,14 +11,17 @@ class CardFactory
 
     private CardBuilder $builder;
 
+    private Card $card;
+
     public function __construct()
     {
         $this->builder = new CardBuilder();
+        $this->card = new Card();
     }
 
     public function create(array $jsonCard): CardInterface
     {
-        $card = new Card();
+        $card = clone $this->card;
         $this->builder->build($card, $jsonCard);
         return $card;
     }
